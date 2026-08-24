a2f110d57772900128fa8a9028ce1a0a07e9a7cb
José Fonseca <jfonseca@aneo.fr>
2026-08-24 11:04:51 +0200

    Fix TiffHolder to fail loudly and avoid uninitialized state

--- include/TiffHolder.hpp
+++ include/TiffHolder.hpp
@@ -2,17 +2,20 @@
 #define TiffHolder_HPP_
 
 #include <iostream>
+#include <stdexcept>
 #include <stdlib.h>
+#include <string>
 #include <tiffio.h>
+#include <vector>
 
 class TiffHolder {
 private:
   TIFF *tiff_to_track = nullptr;
 
 public:
-  uint32_t layers;
-  uint32_t width;
-  uint32_t height;
+  uint32_t layers = 0;
+  uint32_t width = 0;
+  uint32_t height = 0;
   std::vector<std::vector<uint32_t>> imageVectors;
   std::string filePath;
 
@@ -25,30 +28,31 @@ public:
 
     tiff_to_track = TIFFOpen(pathAsCString, "r");
 
-    if (tiff_to_track) {
-
-      // Get image information
-      TIFFGetField(tiff_to_track, TIFFTAG_IMAGEWIDTH, &width);
-      TIFFGetField(tiff_to_track, TIFFTAG_IMAGELENGTH, &height);
-      layers = TIFFNumberOfDirectories(tiff_to_track);
-
-      for (int l = 0; l < layers; l++) {
-
-        TIFFSetDirectory(tiff_to_track, l);
-
-        // Allocate memory for current image layer
-        std::vector<uint32_t> raster(width * height);
-
-        // Read the image
-        if (TIFFReadRGBAImage(tiff_to_track, width, height, raster.data())) {
-          // Add the image data to the vector
-          imageVectors.push_back(std::move(raster));
-        }
-      }
-
-      // Close TIFF file
-      TIFFClose(tiff_to_track);
+    if (!tiff_to_track) {
+      throw std::runtime_error("Could not open TIFF file: " + filePath);
     }
+
+    // Get image information
+    TIFFGetField(tiff_to_track, TIFFTAG_IMAGEWIDTH, &width);
+    TIFFGetField(tiff_to_track, TIFFTAG_IMAGELENGTH, &height);
+    layers = TIFFNumberOfDirectories(tiff_to_track);
+
+    for (int l = 0; l < layers; l++) {
+
+      TIFFSetDirectory(tiff_to_track, l);
+
+      // Allocate memory for current image layer
+      std::vector<uint32_t> raster(width * height);
+
+      // Read the image
+      if (TIFFReadRGBAImage(tiff_to_track, width, height, raster.data())) {
+        // Add the image data to the vector
+        imageVectors.push_back(std::move(raster));
+      }
+    }
+
+    // Close TIFF file
+    TIFFClose(tiff_to_track);
   }
 
   ~TiffHolder(){};
